4bb7067680285f4808fec9a41a318d33bebe3f8c
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-01-12 09:41:19 +0700

    Update GausianElimination.cpp

--- GausianElimination.cpp
+++ GausianElimination.cpp
@@ -1,33 +1,48 @@
 #include<bits/stdc++.h>
 #define pb push_back
 using namespace std;
+int n;
+vector<vector<double> > mat(1000);
+vector<double> bb;
 void solveNdim(vector<vector<double> > &v, int n, vector<double> &b){
-
+	
 	for(int i=0; i<n; i++){
+		if(!v[i][i]){
+			for(int j=i+1; j<n; j++){
+				if(v[j][i]){
+					swap(v[i],v[j]);
+					swap(b[i],b[j]);
+					break;
+				}
+			}
+		}
+		
 		for(int j=0; j<n; j++){
 			for(int h=0; h<n; h++){
 				cout<<v[j][h]<<" ";
 			}
-			cout<<b[i];
+			cout<<b[j];
 			cout<<endl;
 		}
 		cout<<"-----"<<endl;
+		
 		for(int j=i+1; j<n; j++){
 			double div=v[j][i]/v[i][i];
 			for(int h=i; h<n; h++){
-				v[j][h]-=div*v[i][i];
+				v[j][h]-=div*v[i][h];
 			}
 			b[j]-=b[i]*div;
 		}
-		
 	}
 	
 	double x[n];
 	for(int i=n-1; i>=0; i--){
-		int sum=0;
+		double sum=0;
+		
 		for(int j=n-1; j>i; j--){
 			sum+=x[j]*v[i][j];
 		}
+		
 		x[i]=(b[i]-sum)/v[i][i];
 	}
 	for(int i=0; i<n; i++){
@@ -36,28 +51,74 @@ void solveNdim(vector<vector<double> > &v, int n, vector<double> &b){
 	cout<<endl;
 	
 }
-int main(){
-
-	cout<<setprecision(1)<<fixed;
-	int n;
-	cin>>n;
-	vector<vector<double> > v(n);
-	vector<double> b(n);	
+void generate(){
+	
+	int MM=10,p=10,coef=10;
+	
+	for(int i=0; i<MM; i++)n=rand()%MM;
+	
+	vector<double> pas;
+	
 	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			double x;
-			cin>>x;
-			v[i].pb(x);
+		int xux=1;
+		for(int j=0; j<5; j++){
+			xux=rand()%p;
+		}
+		pas.pb(xux);
+	}
+	mat.resize(n);
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n;  j++){
+			int xux;int cc=2*coef;
+			for(int h=0; h<5; h++){
+				xux=rand()%cc;
+			}
+			if(xux==coef)xux++;
+			mat[i].pb(xux-coef);
 		}
 	}
-	for(double i=0; i<n; i++)cin>>b[i];
-	solveNdim(v,n,b);
+	
+	for(int i=0; i<n; i++){
+		int sum=0;
+		
+		for(int j=0; j<n; j++){
+			sum+=mat[i][j]*pas[j];
+		}
+		
+		bb.pb(sum);
+	}
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cout<<mat[i][j]<<" ";
+		}
+		cout<<bb[i]<<endl;
+	}
+	cout<<"----"<<endl;
+	
+	cout<<"Intended answers:"<<endl;
+	for(int i=0; i<n; i++){
+		cout<<pas[i]<<" ";
+	}
+	cout<<endl<<"-----"<<endl;
 }
-/*
-3
-1 1 1
-2 -1 -1
-6 -4 2
+int main(){
 
-3 0 4
-*/
+	cout<<setprecision(2)<<fixed;
+	generate();
+	
+	vector<vector<double> > v(n);
+	vector<double> b(n);	
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			v[i].pb(mat[i][j]);
+		}
+	}
+	
+	for(double i=0; i<n; i++)b[i]=bb[i];
+	
+	solveNdim(v,n,b);
+	
+}
